637bcc33c9213c5262443447a4c0f340d46e03fa
Lauren George <lgeorge@idmod.org>
2019-11-12 13:41:21 -0800

    R Wrappers: Updated HINT_A and added HINT B and C R scripts

--- Scenarios/Generic/HINT_AgeAndAccess/C_AccessTargetedVaccine/HINT_C.R
+++ Scenarios/Generic/HINT_AgeAndAccess/C_AccessTargetedVaccine/HINT_C.R
@@ -3,7 +3,7 @@
 ########################
 
 # Set working directory
-setwd(file.path(PROJECT_ROOT, 'Scenarios', 'Generic', 'HINT_AgeAndAccess', 'A_BaselineOutbreak'))
+setwd(file.path(PROJECT_ROOT, 'Scenarios', 'Generic', 'HINT_AgeAndAccess', 'C_AccessTargetedVaccine'))
 
 ##
 ## Preamble
@@ -63,38 +63,88 @@ overlay_temp <- overlay
 ## We also want to edit the connectivity matricies as 
 ## described in the slides
 AccessibilityMatrix <- rbind(c(.9,.1),
-                             c(.1,.5))
+														 c(.1,.5))
 
 AgeMatrix <- rbind(c(.4,.2,.1,.3),
-                   c(.2,.4,.1,.3),
-                   c(.1,.2,.6,.1),
-                   c(.1,.1,.1,.7))
+									 c(.2,.4,.1,.3),
+									 c(.1,.2,.6,.1),
+									 c(.1,.1,.1,.7))
 
 ## We need to update the connectivity matricies
 for (i in 1:2){
-  for (j in 1:2){
-    overlay_temp$Defaults$`IndividualProperties`[[1]]$`TransmissionMatrix`$Matrix[[i]][[j]] <- AccessibilityMatrix[i,j]
-  }
+	for (j in 1:2){
+		overlay_temp$Defaults$`IndividualProperties`[[1]]$`TransmissionMatrix`$Matrix[[i]][[j]] <- AccessibilityMatrix[i,j]
+	}
 }
 
 for (i in 1:4){
-  for (j in 1:4){
-    overlay_temp$Defaults$`IndividualProperties`[[2]]$`TransmissionMatrix`$Matrix[[i]][[j]] <- AgeMatrix[i,j]
-  }
+	for (j in 1:4){
+		overlay_temp$Defaults$`IndividualProperties`[[2]]$`TransmissionMatrix`$Matrix[[i]][[j]] <- AgeMatrix[i,j]
+	}
 }
 
 ## We now place the temporary overlay file in the correct location
 write(toJSON(overlay_temp, indent = 2), file=file.path(EMOD_INPUT_ROOT,"hint_ageandaccess_overlay.temp.json"))
 
+##################
+##              ##
+## Intervention ##
+##              ##
+################## 
+
+## We will be also editing the campaign file for the intervention
+## We need to update the file name in the temporary
+## config with out temporary campaign filename
+config_temp$`parameters`$Campaign_Filename <- "campaign.temp.json"
+
+## We read in the campaign file and again, make a new temporary one
+campaign <- fromJSON(file = "campaign.json", simplify=FALSE)
+campaign_temp <- campaign
+
+## There are a number of configurable components that go into the
+## intervention. This intervention is age based. 
+## Here we will provide the capacity to alter a few. 
+## Note: this is a small subset of the options available
+##
+
+## First, we can alter the features of the intervention
+
+## How effective is the intervention?
+campaign_temp$Events[[2]]$Event_Coordinator_Config$Intervention_Config$Waning_Config$Initial_Effect <- 0.95
+
+## We will turn this off... for now
+## campaign_temp$Events[[2]]$Event_Coordinator_Config$Target_Age_Max <- 10
+## campaign_temp$Events[[2]]$Event_Coordinator_Config$Target_Age_Min <- 6
+
+## For the population we will target with the intervention
+## (which could be everyone), what fraction of them will we reach?
+campaign_temp$Events[[2]]$Event_Coordinator_Config$Demographic_Coverage <- 0.95
+
+## Once we find a person, they may not be receptive to the intervention,
+## or they may just refuse it. For what fraction of those who we reach
+## is the intervention effective?
+campaign_temp$Events[[2]]$Event_Coordinator_Config$Intervention_Config$Vaccine_Take <- 0.95
+
+## Finally, when do we want to start the intervention? 
+## Note: for simplicity, we will execute our intervention
+## within the stipulations made above on a single day
+## (this is a very simplifying assumption that can be 
+## remidied).
+campaign_temp$Events[[2]]$Start_Day <- 20
+
+
+## Finally, we write our campaign file
+write(toJSON(campaign_temp, indent = 2), file="campaign.temp.json")
+
 ## Different model runs will result in different output
 ## due to the stochasticity within the model. We can observe
 ## this stochasticity by running the model a number of times.
 ## You can increase this number, but the code will take longer to run
-NumSims <- 15
+NumSims <- 30
 
 ## We need to set up locations to store output
 Ss <- Is <- Rs <- matrix(NA, NumSims, Duration)
-AllIs <- AllNs <- array(NA, dim=c(2,4,NumSims,Duration))
+CumSumIs <- NewIs <- AllIs <- AllNs <- array(NA, dim=c(2,4,NumSims,Duration))
 
 ## Here is where we will run and re-run EMOD using the 
 ## temporary config file
@@ -111,6 +161,9 @@ for (i in 1:NumSims){
 			AllNs[j,k,i,] <- output_PR$Channels[tmptxt][[1]]$Data
 			tmptxt <- sprintf("Infected:Accessibility:%s,Age_Bin:%s", AccBins[j], AgeBins[k])
 			AllIs[j,k,i,] <- output_PR$Channels[tmptxt][[1]]$Data
+			tmptxt <- sprintf("New Infections:Accessibility:%s,Age_Bin:%s", AccBins[j], AgeBins[k])
+			NewIs[j,k,i,] <- output_PR$Channels[tmptxt][[1]]$Data
+			CumSumIs[j,k,i,] <- cumsum(NewIs[j,k,i,])
 		}
 	}
 
@@ -125,10 +178,12 @@ for (i in 1:NumSims){
 ## increase the chance of an outbreak. For now, let's toss those
 ## failed pathogen invasions.
 
-Toss <- which(apply(Ss,1,min)==1)
-Ss <- Ss[-Toss,]
-Is <- Is[-Toss,]
-Rs <- Rs[-Toss,]
+Toss <- which(apply(Is, 1, max)==0)
+if (length(Toss)){
+  Ss <- Ss[-Toss,]
+  Is <- Is[-Toss,]
+  Rs <- Rs[-Toss,]
+}
 
 ## We can calculate summary statistics of the model runs
 ## Here we are calculating the 2.5th, 50th, and 97.5th
@@ -137,6 +192,7 @@ SOut <-  apply(Ss,2,quantile,pro=c(0.025,.5,.975))
 IOut <-  apply(Is,2,quantile,pro=c(0.025,.5,.975))
 ROut <-  apply(Rs,2,quantile,pro=c(0.025,.5,.975))
 
+par(mfrow=c(1,1))
 ## This plots the summary output
 plot(1, type='n', ylim=c(0,1),xlim=c(1,max(which(diff(ROut[2,]) != 0))),xlab="Day",ylab="Fraction of population")
 mtext(bquote(beta == .(beta)*", and " ~ gamma == .(gamma)),3)
@@ -163,22 +219,49 @@ legend("topright",col=COLs,lwd=2,legend=c("Susceptible","Infectious","Recovered"
 
 
 
-
 ## This plots the raw output by group
+par(mfrow=c(2,4))
+for (i in 1:2){
+	for (j in 1:4){
+	  if (length(Toss)){
+	    plot(apply(AllIs[i,j,-Toss,]/AllNs[i,j,-Toss,],2,mean),ylim=c(0,1),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
+	         col=COLs[2],xlab="Day",ylab="Fraction of population")
+	  } else {
+	    plot(apply(AllIs[i,j,,]/AllNs[i,j,,],2,mean),ylim=c(0,1),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
+	         col=COLs[2],xlab="Day",ylab="Fraction of population")
+	  }
+		mtext(paste(AgeLabels[j], AccBins[i],sep=" & "))
+		for (k in setdiff(1:NumSims,Toss)){
+			lines(AllIs[i,j,k,]/AllNs[i,j,k,],col=adjustcolor(COLs[2],alpha=.1))
+		}
+	}
+}
+
+## This plots the cumulative number of new infections
 par(mfrow=c(2,4))
 for (i in 1:2){
   for (j in 1:4){
     if (length(Toss)){
-      plot(apply(AllIs[i,j,-Toss,]/AllNs[i,j,-Toss,],2,mean),ylim=c(0,1),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
-           col=COLs[2],xlab="Day",ylab="Fraction of population")
+      plot(apply(CumSumIs[i,j,-Toss,],2,mean),ylim=c(0,max(CumSumIs)),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
+           col=COLs[2],xlab="Day",ylab="Cumulative number of infections")
     } else {
-      plot(apply(AllIs[i,j,,]/AllNs[i,j,,],2,mean),ylim=c(0,1),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
-           col=COLs[2],xlab="Day",ylab="Fraction of population")
+      plot(apply(CumSumIs[i,j,,],2,mean),ylim=c(0,max(CumSumIs)),type='l',xlim=c(1,max(which(diff(ROut[2,]) != 0))),
+           col=COLs[2],xlab="Day",ylab="Cumulative number of infections")
     }
     mtext(paste(AgeLabels[j], AccBins[i],sep=" & "))
     for (k in setdiff(1:NumSims,Toss)){
-      lines(AllIs[i,j,k,]/AllNs[i,j,k,],col=adjustcolor(COLs[2],alpha=.1))
+      lines(CumSumIs[i,j,k,],col=adjustcolor(COLs[2],alpha=.1))
     }
+  }
+}
+
+## Finally, some estimate of the total outbreak size
+par(mfrow=c(2,4))
+for (i in 1:2){
+  for (j in 1:4){
+    hist(CumSumIs[i,j,,dim(CumSumIs[i,j,,])[2]],
+           col=COLs[2],xlab="Day",ylab="Total number of infections",main="")
+    mtext(paste(AgeLabels[j], AccBins[i],sep=" & "))
   }
 }
 
